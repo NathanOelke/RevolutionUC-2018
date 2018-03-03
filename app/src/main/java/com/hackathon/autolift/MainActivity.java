a8cc9520ab1e4a46bf270c52ec5800b2c0f33fc1
Nathan Oelke <oelke047@umn.edu>
2018-03-03 15:38:41 -0600

    Added Myo Android SDK to Android Studio project

--- app/src/main/java/com/hackathon/autolift/MainActivity.java
+++ app/src/main/java/com/hackathon/autolift/MainActivity.java
@@ -1,13 +1,54 @@
 package com.hackathon.autolift;
 
+import com.thalmic.myo.*;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DeviceListener mListener = new AbstractDeviceListener() {
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            Toast.makeText(MainActivity.this, "Myo Connected!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onDisconnect(Myo myo, long timestamp) {
+            Toast.makeText(MainActivity.this, "Myo Disconnected!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPose(Myo myo, long timestamp, Pose pose) {
+            Toast.makeText(MainActivity.this, "Pose: " + pose, Toast.LENGTH_SHORT).show();
+
+            //TODO: Do something awesome.
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Hub for Myo
+        Hub hub = Hub.getInstance();
+        if (!hub.init(this)) {
+            System.out.println("\n\nCouldn't initialize Hub");
+            finish();
+            return;
+        }
+        System.out.println("\n\nHub Loaded Successfully");
+
+        // Disable standard Myo locking policy. All poses will be delivered.
+        hub.setLockingPolicy(Hub.LockingPolicy.NONE);
+
+        // Next, register for DeviceListener callbacks.
+        hub.addListener(mListener);
+        
+        // Finally, scan for Myo devices and connect to the first one found that is very near.
+        hub.attachToAdjacentMyo();
     }
 }
